b7d7fd442ecab9cf9ced04186ffe68b8f455ca2f
parkyes90 <parkyes90@gmail.com>
2020-04-01 10:52:18 +0900

    improve solution 594

--- leetcode/501~600/594. Longest Harmonious Subsequence.py
+++ leetcode/501~600/594. Longest Harmonious Subsequence.py
@@ -4,24 +4,16 @@ from typing import List
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
         count_map = {}
+        maximum = 0
         for n in nums:
             if n not in count_map:
                 count_map[n] = 1
             else:
                 count_map[n] += 1
-        count_keys = sorted(count_map.keys())
-        combinations = []
-        for i in range(len(count_keys) - 1):
-            if count_keys[i + 1] - count_keys[i] < 2:
-                combinations.append((count_keys[i], count_keys[i + 1]))
-
-        if not combinations:
-            return 0
-        maximum = 0
-        for combination in combinations:
-            key1, key2 = combination
-            if count_map[key1] + count_map[key2] > maximum:
-                maximum = count_map[key1] + count_map[key2]
+            if count_map.get(n + 1):
+                maximum = max(maximum, count_map[n] + count_map[n + 1])
+            if count_map.get(n - 1):
+                maximum = max(maximum, count_map[n] + count_map[n - 1])
 
         return maximum
 
