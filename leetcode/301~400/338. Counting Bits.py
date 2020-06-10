c8b06832af4e27e7218f1b81a489e584066a2d01
parkyes90 <parkyes90@gmail.com>
2020-06-10 16:03:51 +0900

    Add problems 338

---
+++ leetcode/301~400/338. Counting Bits.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    answer = s.countBits(2)
+    print(answer)
