67e70dd49b607f9026699ba7499f7b4ae6e6c3a2
parkyes90 <parkyes90@gmail.com>
2020-06-27 10:07:56 +0900

    Add problems 1110

---
+++ leetcode/1101~1200/1110. Delete Nodes And Return Forest.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def delNodes(self, root: TreeNode, to_delete: List[int]) -> List[TreeNode]:
+        pass
+
+
+if __name__ == "__main__":
+    pass
