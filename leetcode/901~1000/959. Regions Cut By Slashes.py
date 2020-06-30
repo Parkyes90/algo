1c9a13967764fb0ba434d1d88543704f8dbf227d
parkyes90 <parkyes90@gmail.com>
2020-07-01 00:03:35 +0900

    Add problems 959

---
+++ leetcode/901~1000/959. Regions Cut By Slashes.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def regionsBySlashes(self, grid: List[str]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    answer = s.regionsBySlashes([" /", "/ "])
