7fed74484c9e2f49894c775db5860c4966f501d0
parkyes90 <parkyes90@gmail.com>
2020-07-03 00:34:47 +0900

    Add problems 406

---
+++ leetcode/401~500/406. Queue Reconstruction by Height.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    answer = s.reconstructQueue(
+        [[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]
+    )
+    print(answer)
