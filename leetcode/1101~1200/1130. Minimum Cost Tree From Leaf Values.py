db2d5e3b8477a37917f66ab824f91d59aaac8aac
parkyes90 <parkyes90@gmail.com>
2020-07-04 11:58:54 +0900

    Add problems 1130

---
+++ leetcode/1101~1200/1130. Minimum Cost Tree From Leaf Values.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def mctFromLeafValues(self, arr: List[int]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    answer = s.mctFromLeafValues([6, 2, 4])
