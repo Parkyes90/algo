e9cf738ea3439451f7ff3e4a9d9d286398f954f3
parkyes90 <parkyes90@gmail.com>
2020-07-14 10:13:43 +0900

    Add problems 951

---
+++ leetcode/901~1000/951. Flip Equivalent Binary Trees.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def flipEquiv(self, root1: TreeNode, root2: TreeNode) -> bool:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    answer = s.flipEquiv()
