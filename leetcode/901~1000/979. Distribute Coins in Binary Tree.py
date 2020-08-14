4b01e15e27f50462f00b22e111757d56b31aabbe
parkyes90 <parkyes90@gmail.com>
2020-08-14 10:09:09 +0900

    Add problems 979

--- leetcode/901~1000/979. Distribute Coins in Binary Tree.py
+++ leetcode/901~1000/979. Distribute Coins in Binary Tree.py
@@ -7,20 +7,8 @@ class TreeNode:
 
 
 class Solution:
-    count = 0
-
-    def recursive(self, node):
-        if not node:
-            return 0
-
-        left = self.recursive(node.left)
-        right = self.recursive(node.right)
-        self.count += abs(left) + abs(right)
-        return node.val + left + right - 1
-
     def distributeCoins(self, root: TreeNode) -> int:
-        self.recursive(root)
-        return self.count
+        pass
 
 
 def create_root(arr, root, index, n):
@@ -33,7 +21,7 @@ def create_root(arr, root, index, n):
 
 if __name__ == "__main__":
     s = Solution()
-    t_arr = [1, 0, 0, None, 3]
-    r = create_root(t_arr, None, 0, len(t_arr))
+    test_arr = [3, 0, 0]
+    r = create_root(test_arr, None, 0, len(test_arr))
     answer = s.distributeCoins(r)
     print(answer)
