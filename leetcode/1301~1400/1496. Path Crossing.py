9b0ed742e7427511ef898c1955f7a8ad103d4f0b
parkyes90 <parkyes90@gmail.com>
2020-08-16 12:58:54 +0900

    Add problems 1496

---
+++ leetcode/1301~1400/1496. Path Crossing.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        direction_map = {
+            "N": 1,
+            "E": 1,
+            "S": -1,
+            "W": -1,
+        }
+        start = (0, 0)
+        visits = {start}
+        for p in path:
+
+            x, y = start
+            direction = direction_map[p]
+            if p in {"N", "S"}:
+                y += direction
+            else:
+                x += direction
+            start = (x, y)
+            if start in visits:
+                return True
+            visits.add(start)
+
+        return False
+
+
+if __name__ == "__main__":
+    s = Solution()
+    answer = s.isPathCrossing("NESWW")
+    print(answer)
