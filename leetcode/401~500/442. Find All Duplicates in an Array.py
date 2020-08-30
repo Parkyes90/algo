c226ebc2ad8a4cce19a7f77511d6ff7499174240
parkyes90 <parkyes90@gmail.com>
2020-08-30 14:01:34 +0900

    Add problems 442

---
+++ leetcode/401~500/442. Find All Duplicates in an Array.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    s.findDuplicates([4, 3, 2, 7, 8, 2, 3, 1])
