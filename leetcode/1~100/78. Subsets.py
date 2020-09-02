c7d893a0cb5273f836463c76f914068c75d3d307
parkyes90 <parkyes90@gmail.com>
2020-09-02 17:02:55 +0900

    Add problems 78

---
+++ leetcode/1~100/78. Subsets.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    answer = s.subsets([1, 2, 3])
