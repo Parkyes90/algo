2ec754dad0c180783f49fe2a0909fed4d68eb431
parkyes90 <parkyes90@gmail.com>
2020-10-02 00:20:38 +0900

    Modify problems 1598

--- leetcode/1501~1600/1598. Crawler Log Folder.py
+++ leetcode/1501~1600/1598. Crawler Log Folder.py
@@ -3,10 +3,17 @@ from typing import List
 
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        pass
+        count = 0
+        for log in logs:
+            if log in {"../"}:
+                if count:
+                    count -= 1
+            elif log not in {"./"}:
+                count += 1
+        return count
 
 
 if __name__ == "__main__":
     s = Solution()
-    answer = s.minOperations([])
+    answer = s.minOperations(["d1/", "../", "../", "../"])
     print(answer)
