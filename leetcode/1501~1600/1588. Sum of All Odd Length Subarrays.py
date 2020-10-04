4f3b572b873a5723267fc33c8c130a910522c908
parkyes90 <parkyes90@gmail.com>
2020-10-04 12:38:02 +0900

    solve problem 1588

--- leetcode/1501~1600/1588. Sum of All Odd Length Subarrays.py
+++ leetcode/1501~1600/1588. Sum of All Odd Length Subarrays.py
@@ -3,4 +3,16 @@ from typing import List
 
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        pass
+        odd_length = 1
+        total = 0
+        while odd_length <= len(arr):
+            for i in range(len(arr) + 1 - odd_length):
+                total += sum(arr[i : i + odd_length])
+            odd_length += 2
+        return total
+
+
+if __name__ == "__main__":
+    s = Solution()
+    ans = s.sumOddLengthSubarrays([10, 11, 12])
+    print(ans)
