ee880dcfc288c33f611d9f1191a22b894397b0fe
parkyes90 <parkyes90@gmail.com>
2020-10-11 12:31:39 +0900

    Add solution 1608

--- leetcode/1601~1700/1608. Special Array With X Elements Greater Than or Equal X.py
+++ leetcode/1601~1700/1608. Special Array With X Elements Greater Than or Equal X.py
@@ -1,12 +1,21 @@
-from typed_ast._ast3 import List
+from typing import List
 
 
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
-        pass
+        x = None
+        for i in range(1, len(nums) + 1):
+            count = 0
+            for n in nums:
+                if n >= i:
+                    count += 1
+            if count == i:
+                x = i
+                break
+        return x if x is not None else -1
 
 
 if __name__ == "__main__":
     s = Solution()
-    answer = s.specialArray([3, 5])
-    assert answer == 2
+    answer = s.specialArray([0, 4, 3, 0, 4])
+    assert answer == 3
