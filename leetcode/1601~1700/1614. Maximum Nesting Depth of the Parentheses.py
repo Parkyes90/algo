12c9a422be169cb3a3e31aa55fa236ff8fe511e9
parkyes90 <parkyes90@gmail.com>
2020-10-14 12:18:55 +0900

    Add solution 1614

--- leetcode/1601~1700/1614. Maximum Nesting Depth of the Parentheses.py
+++ leetcode/1601~1700/1614. Maximum Nesting Depth of the Parentheses.py
@@ -1,9 +1,17 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
-        pass
+        maximum = 0
+        current = 0
+        for el in s:
+            if el == "(":
+                current += 1
+            maximum = max(maximum, current)
+            if el == ")":
+                current -= 1
+        return maximum
 
 
 if __name__ == "__main__":
     s = Solution()
-    answer = s.maxDepth()
+    answer = s.maxDepth("1")
     print(answer)
