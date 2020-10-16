7ad0f0f89b0ccf64b83b87073eabd7864ec8917c
parkyes90 <parkyes90@gmail.com>
2020-10-16 17:48:14 +0900

    solve solution 1603

--- leetcode/1601~1700/1603. Design Parking System.py
+++ leetcode/1601~1700/1603. Design Parking System.py
@@ -1,9 +1,24 @@
 class ParkingSystem:
-
     def __init__(self, big: int, medium: int, small: int):
+        self.system = {1: big, 2: medium, 3: small}
 
     def addCar(self, carType: int) -> bool:
-        pass
+        if self.system[carType] < 1:
+            return False
+        self.system[carType] -= 1
+        return True
+
+
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)
 # param_1 = obj.addCar(carType)
+if __name__ == "__main__":
+    parking = ParkingSystem(1, 1, 0)
+    print(
+        [
+            parking.addCar(1),
+            parking.addCar(2),
+            parking.addCar(3),
+            parking.addCar(1),
+        ]
+    )
