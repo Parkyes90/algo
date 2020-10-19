4141d74f0af8c5736b969a39e252bcac68c63f1c
parkyes90 <parkyes90@gmail.com>
2020-10-19 20:37:48 +0900

    add problem 1619

---
+++ leetcode/1601~1700/1619. Mean of Array After Removing Some Elements.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def trimMean(self, arr: List[int]) -> float:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    answer = s.trimMean(
+        [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3]
+    )
+    assert answer == 2.00000
