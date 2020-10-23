3b038722929b73ef41c5e685a7e3ddd8eca8d33e
parkyes90 <parkyes90@gmail.com>
2020-10-23 20:38:09 +0900

    add problem 1624

---
+++ leetcode/1601~1700/1624. Largest Substring Between Two Equal Characters.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    answer = s.maxLengthBetweenEqualCharacters("aa")
