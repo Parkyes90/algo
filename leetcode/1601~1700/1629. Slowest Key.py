d492d8c2902f3e2a1eafe2712476cee73eea26af
parkyes90 <parkyes90@gmail.com>
2020-10-27 18:49:57 +0900

    add problem 1629

---
+++ leetcode/1601~1700/1629. Slowest Key.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    answer = s.slowestKey([12, 23, 36, 46, 62], "spuda")
+    assert answer == "a"
