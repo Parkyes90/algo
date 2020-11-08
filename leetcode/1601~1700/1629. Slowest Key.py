e378bcab09c14e3d3519d7c01fa93de9d8a7c5d1
parkyes90 <parkyes90@gmail.com>
2020-11-08 15:28:20 +0900

    solve problem 1629

--- leetcode/1601~1700/1629. Slowest Key.py
+++ leetcode/1601~1700/1629. Slowest Key.py
@@ -3,10 +3,25 @@ from typing import List
 
 class Solution:
     def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
-        pass
+        times = [0, *releaseTimes]
+        maximum = 0
+        for i in range(1, len(times)):
+            duration = times[i] - times[i - 1]
+            if duration > maximum:
+                maximum = duration
+        indexes = []
+        for i in range(1, len(times)):
+            duration = times[i] - times[i - 1]
+            if duration == maximum:
+                indexes.append(i - 1)
+        keys = []
+        for i in indexes:
+            keys.append(keysPressed[i])
+        keys.sort(reverse=True)
+        return keys[0]
 
 
 if __name__ == "__main__":
     s = Solution()
-    answer = s.slowestKey([12, 23, 36, 46, 62], "spuda")
-    assert answer == "a"
+    answer = s.slowestKey([9, 29, 49, 50], "cbcd")
+    assert answer == "c"
