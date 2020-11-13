e6f257974bb9dd1098eba31aea08942ed3dd828b
parkyes90 <parkyes90@gmail.com>
2020-11-13 15:07:14 +0900

    solve problem 1640

--- leetcode/1601~1700/1640. Check Array Formation Through Concatenation.py
+++ leetcode/1601~1700/1640. Check Array Formation Through Concatenation.py
@@ -3,11 +3,22 @@ from typing import List
 
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
-        pass
+        for piece in pieces:
+            start = piece[0]
+            length = len(piece)
+            try:
+                index = arr.index(start)
+                arr_piece = arr[index : index + length]
+                if arr_piece != piece:
+                    return False
+            except ValueError:
+                return False
+
+        return True
 
 
 if __name__ == "__main__":
     s = Solution()
-    answer = s.canFormArray([85], [[85]])
+    answer = s.canFormArray([91, 4, 64, 78], [[78], [4, 64], [91]])
 
-    assert answer == True
+    assert answer
