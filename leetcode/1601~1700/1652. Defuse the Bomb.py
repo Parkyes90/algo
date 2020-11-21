f00b072d1a77b54165cb8faa23a26ff3504fa109
parkyes90 <parkyes90@gmail.com>
2020-11-21 14:32:17 +0900

    add problem 1652

---
+++ leetcode/1601~1700/1652. Defuse the Bomb.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    answer = s.decrypt()
+    assert answer == [12, 10, 16, 13]
