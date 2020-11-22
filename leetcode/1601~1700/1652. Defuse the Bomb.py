316485f5fff159fa41d3f53a10e3b4a32754f225
parkyes90 <parkyes90@gmail.com>
2020-11-22 14:21:17 +0900

    solve problem 1652

--- leetcode/1601~1700/1652. Defuse the Bomb.py
+++ leetcode/1601~1700/1652. Defuse the Bomb.py
@@ -3,10 +3,28 @@ from typing import List
 
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
-        pass
+        n = len(code)
+        if k == 0:
+            return [0] * n
+        ret = []
+
+        for i in range(n):
+            el = 0
+            if k > 0:
+                for j in range(i + 1, i + k + 1):
+                    idx = j if j < n else j - n
+                    el += code[idx]
+            else:
+                for j in range(1, abs(k) + 1):
+                    idx = i - j
+                    idx = idx if idx > -1 else n + idx
+                    print(idx)
+                    el += code[idx]
+            ret.append(el)
+        return ret
 
 
 if __name__ == "__main__":
     s = Solution()
-    answer = s.decrypt()
-    assert answer == [12, 10, 16, 13]
+    answer = s.decrypt([1, 2, 3, 4], 0)
+    print(answer)
