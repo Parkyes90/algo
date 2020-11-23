be59640c278aff012fc1e890d827e2feb62c43bb
parkyes90 <parkyes90@gmail.com>
2020-11-23 19:06:58 +0900

    add problem 1662

---
+++ leetcode/1601~1700/1662. Check If Two String Arrays are Equivalent.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    answer = s.arrayStringsAreEqual(["ab", "c"], ["a", "bc"])
+    assert answer is True
