31ee8b2cb7e809560de6e5d4a7bae41b4967a162
parkyes90 <parkyes90@gmail.com>
2020-11-24 19:44:24 +0900

    solve problem 1662

--- leetcode/1601~1700/1662. Check If Two String Arrays are Equivalent.py
+++ leetcode/1601~1700/1662. Check If Two String Arrays are Equivalent.py
@@ -3,7 +3,13 @@ from typing import List
 
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        pass
+        w1 = ""
+        w2 = ""
+        for piece in word1:
+            w1 += piece
+        for piece in word2:
+            w2 += piece
+        return w1 == w2
 
 
 if __name__ == "__main__":
