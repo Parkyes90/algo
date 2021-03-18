9c15034d21ddf0b8fe4300655a36afa564fac5c6
parkyes90 <parkyes90@gmail.com>
2021-03-19 02:00:36 +0900

    right answer - 1

--- local/scofe.py
+++ local/scofe.py
@@ -1,29 +1,26 @@
 def solution(a1, a2, a3):
-    count = 0
-    total_remain = 0
+    counts = []
     minimum = min(a3)
     minimum_index = a3.index(minimum)
-    offsets = sorted([minimum_index + 1, a1 - minimum_index])
-    first_cover_length, second_cover_length = offsets
-
-    def sum_count(length, divider):
-        nonlocal count, total_remain
-        value, r = divmod(length, divider)
-        count += value
-        total_remain += r
-        if r:
+    available_ranges = []
+    for i in range(a2):
+        start_index = minimum_index - i
+        end_index = start_index + a2 - 1
+        if minimum_index - i >= 0 and end_index < a1:
+            available_ranges.append((minimum_index - i, end_index))
+    for r in available_ranges:
+        count = 1
+        left, right = r
+        left_value, left_remain = divmod(left, a2 - 1)
+        count += left_value
+        if left_remain:
             count += 1
-
-    if first_cover_length <= a2:
-        remain = a2 - first_cover_length
-        second_cover_length -= remain
-        count += 1
-    else:
-        sum_count(first_cover_length - 1, a2 - 1)
-    sum_count(second_cover_length - 1, a2 - 1)
-    if total_remain >= a2 - 1:
-        count -= 1
-    return count
+        right_value, right_remain = divmod(a1 - right - 1, a2 - 1)
+        count += right_value
+        if right_remain:
+            count += 1
+        counts.append(count)
+    return min(counts)
 
 
 if __name__ == "__main__":
