9d9016cd93ff31aee7d8c7203d8aeee4001a4237
parkyes90 <parkyes90@gmail.com>
2021-07-14 10:41:45 +0900

    Add problem 1929

---
+++ leetcode/1901~2000/1929. Concatenation of Array.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    ans = s.getConcatenation([1, 2, 1])
+    assert ans == [1, 2, 1, 1, 2, 1]
