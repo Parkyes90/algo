34645938f2df616f0448b568f3ed5d692d5f500f
parkyes90 <parkyes90@gmail.com>
2022-05-24 12:36:15 +0900

    solve 2160 with sorting

--- leetcode/2101~2200/2160. Minimum Sum of Four Digit Number After Splitting Digits.py
+++ leetcode/2101~2200/2160. Minimum Sum of Four Digit Number After Splitting Digits.py
@@ -19,6 +19,13 @@ class Solution:
         return result
 
     def minimumSum(self, num: int) -> int:
+        str_num = str(num)
+        sorted_str_num = sorted(str_num)
+        return int(sorted_str_num[0] + sorted_str_num[3]) + int(
+            sorted_str_num[1] + sorted_str_num[2]
+        )
+
+    def minimumSum2(self, num: int) -> int:
         num_pairs = set()
         indexes = {0, 1, 2, 3}
         str_num = str(num)
