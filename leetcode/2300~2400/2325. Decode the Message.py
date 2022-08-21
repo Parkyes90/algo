4c36b34850e0f0687245863391afcb28850e5c69
Gavin <parkyes90@gmail.com>
2022-08-21 13:51:05 +0900

    Create 2325. Decode the Message.py

---
+++ leetcode/2300~2400/2325. Decode the Message.py
@@ -0,0 +1,19 @@
+from collections import OrderedDict
+
+class Solution:
+    def decodeMessage(self, key: str, message: str) -> str:
+        key_map = OrderedDict()
+        start = ord("a")
+        for letter in key:
+            if letter in key_map or letter == " ":
+                continue
+            key_map[letter] = chr(start)
+            start += 1
+            
+        output = ""
+        for letter in message:
+            if letter == " ":
+                output += " "
+            else:
+                output += key_map[letter]
+        return output
