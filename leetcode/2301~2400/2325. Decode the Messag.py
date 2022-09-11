e370722d164be18eed46667e28ef12eb301e2067
parkyes90 <parkyes90@gmail.com>
2022-09-11 10:23:53 +0900

    apply black

--- leetcode/2301~2400/2325. Decode the Messag.py
+++ leetcode/2301~2400/2325. Decode the Messag.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 
+
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
         key_map = OrderedDict()
@@ -9,7 +10,7 @@ class Solution:
                 continue
             key_map[letter] = chr(start)
             start += 1
-            
+
         output = ""
         for letter in message:
             if letter == " ":
