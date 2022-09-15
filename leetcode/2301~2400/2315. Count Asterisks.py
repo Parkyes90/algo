f923957229b41f66b055b85a2aed91abf8a58a1b
parkyes90 <parkyes90@gmail.com>
2022-09-15 19:16:39 +0900

    solve 2315

--- leetcode/2301~2400/2315. Count Asterisks.py
+++ leetcode/2301~2400/2315. Count Asterisks.py
@@ -1,9 +1,19 @@
 class Solution:
     def countAsterisks(self, s: str) -> int:
-        pass
+        split = s.split("|")
+        count = 0
+        for part in split[::2]:
+            count += part.count("*")
+        return count
 
 
 if __name__ == "__main__":
     solution = Solution()
     answer = solution.countAsterisks("l|*e*et|c**o|*de|")
     assert answer == 2
+
+    answer = solution.countAsterisks("iamprogrammer")
+    assert answer == 0
+
+    answer = solution.countAsterisks("yo|uar|e**|b|e***au|tifu|l")
+    assert answer == 5
