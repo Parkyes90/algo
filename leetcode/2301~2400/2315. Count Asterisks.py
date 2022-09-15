970219910dfbab0c6c6e81413a36a031e873ffb9
parkyes90 <parkyes90@gmail.com>
2022-09-15 19:09:53 +0900

    add 2315

---
+++ leetcode/2301~2400/2315. Count Asterisks.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countAsterisks(self, s: str) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    answer = solution.countAsterisks("l|*e*et|c**o|*de|")
+    assert answer == 2
