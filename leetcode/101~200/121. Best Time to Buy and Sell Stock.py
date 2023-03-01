61f32896fd7a69bd33ddec2ded848ebe07df9a88
parkyes90 <parkyes90@gmail.com>
2023-03-01 20:42:56 +0900

    Solve 121

---
+++ leetcode/101~200/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = float("inf")
+
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+
+        return max_profit
+
+
+if __name__ == "__main__":
+    s = Solution()
+    assert s.maxProfit([7, 1, 5, 3, 6, 4]) == 5
+    assert s.maxProfit([7, 6, 4, 3, 1]) == 0
