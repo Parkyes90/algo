4f7a44178889d8815ca1cf7136441b15fe1035ee
parykes90 <parkyes90@gmail.com>
2023-03-28 23:28:49 +0900

    Solve 2399

---
+++ leetcode/2301~2400/2399. Check Distances Between Same Letters.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def checkDistances(self, s: str, distance: List[int]) -> bool:
+        exclude = set()
+        for index, letter in enumerate(s):
+            if letter in exclude:
+                continue
+
+            distance_index = ord(letter) - 97
+            letter_distance = distance[distance_index]
+            try:
+                if letter != s[index + letter_distance + 1]:
+                    return False
+            except IndexError:
+                return False
+            exclude.add(letter)
+        return True
+
+
+if __name__ == "__main__":
+    assert Solution().checkDistances("abaccb",
+                                     [1, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                      0]) is True
+    assert Solution().checkDistances("aa",
+                                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                      0]) is False
