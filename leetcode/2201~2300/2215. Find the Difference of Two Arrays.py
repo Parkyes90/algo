74372bcf247b2bb248a2bb0dc5b959f68010896f
parykes90 <parkyes90@gmail.com>
2023-03-28 23:49:38 +0900

    Solve 2215

---
+++ leetcode/2201~2300/2215. Find the Difference of Two Arrays.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        answer = []
+        nums2_set = set(nums2)
+        nums1_set = set(nums1)
+        answer.append(list(nums1_set.difference(nums2_set)))
+        answer.append(list(nums2_set.difference(nums1_set)))
+        return answer
+
+
+if __name__ == '__main__':
+    assert Solution().findDifference([1, 2, 3], [2, 4, 6]) == [[1, 3], [4, 6]]
+    assert Solution().findDifference([1, 2, 3, 3], [1, 1, 2, 2]) == [[3], []]
