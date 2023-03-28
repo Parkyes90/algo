7a60321fa5b4723bc29d6f6e902f8f106e73b3bb
parykes90 <parkyes90@gmail.com>
2023-03-28 22:51:53 +0900

    Solve 1876

---
+++ leetcode/1801~1900/1876. Substrings of Size Three with Distinct Characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        if len(s) < 3:
+            return 0
+
+        count = 0
+
+        for i in range(len(s) - 2):
+            substring = s[i:i + 3]
+            if len(substring) == len(set(substring)):
+                count += 1
+        return count
+
+
+if __name__ == "__main__":
+    assert Solution().countGoodSubstrings("xyzzaz") == 1
+    assert Solution().countGoodSubstrings("aababcabc") == 4
